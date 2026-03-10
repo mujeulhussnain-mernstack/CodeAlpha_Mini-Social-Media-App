4262141db6b37300803731cb980a19af55b2ab22
mujeulhussnain-mernstack <mujeulhussnain@gmail.com>
2026-03-10 17:43:45 +0500

    frontend page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,39 @@
 import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import MainLayout from "./components/MainLayout";
+import Feed from "./pages/Feed";
+import AllUsers from "./pages/AllUsers";
+import SuggestedUsers from "./pages/SuggestedUsers";
+import CreatePost from "./pages/CreatePost";
+import EditProfile from "./pages/EditProfile";
+import GetProfile from "./pages/GetProfile";
 
+import NotFound from "./pages/NotFound";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Feed /> },
+      { path: "/search", element: <AllUsers /> },
+      { path: "/create", element: <CreatePost /> },
+      { path: "/suggestedusers", element: <SuggestedUsers /> },
+      { path: "/editprofile", element: <EditProfile /> },
+      { path: "/profile/:id", element: <GetProfile /> },
+    ],
+  },
+  { path: "*", element: <NotFound /> },
+]);
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/GetProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetProfile = () => {
+  return (
+    <div>GetProfile</div>
+  )
+}
+
+export default GetProfile
